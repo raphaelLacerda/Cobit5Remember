72c48b0de5a5c10542a5ec4b0b36c4aad2614684
Raphael Lacerda <lacerdaph@gmail.com>
2016-01-29 19:10:57 -0200

    27002 e 27005

---
+++ processos/27005.js
@@ -0,0 +1,199 @@
+var processos = [
+    {
+        nome: 'Especificação de Critérios Básicos',
+        grupo: 'DEFINIÇÃO-CONTEXTO'
+    },
+    {
+        nome: 'Especificação de Escopo',
+        grupo: 'DEFINIÇÃO-CONTEXTO'
+    },
+    {
+        nome: 'Especificação da Organização',
+        grupo: 'DEFINIÇÃO-CONTEXTO'
+    },
+    {
+        nome: 'Avaliação / impacto / aceitação',
+        grupo: 'DEFINIÇÃO-CONTEXTO'
+    },
+    {
+        nome: 'Definição dos papéis e responsabilidades',
+        grupo: 'DEFINIÇÃO-CONTEXTO'
+    },
+    {
+        nome: 'Identificar, análise/estimar e avaliar o risco',
+        grupo: 'AVALIAÇÃO-DE-RISCOS-PROCESSO-GERAL'
+    },
+    {
+        nome: 'Identificar, quantificar e priorizar os riscos',
+        grupo: 'AVALIAÇÃO-DE-RISCOS-PROCESSO-GERAL'
+    },
+    
+    {
+        nome: 'AA-CVC',
+        grupo: 'IDENTIFICAÇÃO-DE-RISCOS'
+    },
+    
+    {
+        nome: 'Ativos',
+        grupo: 'IDENTIFICAÇÃO-DE-RISCOS'
+    },
+    {
+        nome: 'Ameaças',
+        grupo: 'IDENTIFICAÇÃO-DE-RISCOS'
+    },
+    {
+        nome: 'Controles',
+        grupo: 'IDENTIFICAÇÃO-DE-RISCOS'
+    },
+    {
+        nome: 'Vulnerabilidades',
+        grupo: 'IDENTIFICAÇÃO-DE-RISCOS'
+    },
+    {
+        nome: 'Identificar CONSEQUÊNCIAS',
+        grupo: 'IDENTIFICAÇÃO-DE-RISCOS'
+    },
+    
+    {
+        nome: 'QQ-CPN',
+        grupo: 'ESTIMATIVA-DE-RISCOS'
+    },
+    
+    
+    {
+        nome: 'qualitativos sem dados históricos (pequena, média, alta)',
+        grupo: 'ESTIMATIVA-DE-RISCOS'
+    },
+    
+    {
+        nome: 'quantitativos com dados históricos',
+        grupo: 'ESTIMATIVA-DE-RISCOS'
+    },
+    
+    {
+        nome: 'lista de incidentes identificados como relevantes, incluindo Consequências',
+        grupo: 'ESTIMATIVA-DE-RISCOS'
+    },
+    
+    {
+        nome: 'probabilidades',
+        grupo: 'ESTIMATIVA-DE-RISCOS'
+    },
+    
+    {
+        nome: 'nível --> magnitude de um risco',
+        grupo: 'ESTIMATIVA-DE-RISCOS'
+    },
+    
+    
+    {
+        nome: 'entrada: lista de riscos com níveis de valores e critérios para avaliação',
+        grupo: 'AVALIAÇÃO-DE-RISCOS'
+    },
+    
+    
+    {
+        nome: 'saída: lista de riscos avaliados, ordenados por prioridade',
+        grupo: 'AVALIAÇÃO-DE-RISCOS'
+    },
+    
+    
+    {
+        nome: 'ordenar prioritariamente de acordo com a importência do risco',
+        grupo: 'AVALIAÇÃO-DE-RISCOS'
+    },
+    
+    
+    {
+        nome: 'Reter / Evitar / Transferir / Reduzir',
+        grupo: 'TRATAMENTO-DE-RISCOS'
+    },
+    {
+        nome: 'Riscos residuais',
+        grupo: 'ACEITAÇÃO-DE-RISCOS'
+    },
+    {
+        nome: 'Trocar informação entre os tomadores de decisão',
+        grupo: 'COMUNICAÇÃO-DE-RISCOS'
+    },
+    {
+        nome: 'Monitoramento dos fatores de riscos',
+        grupo: 'MONITORAÇÃO-REVISÃO'
+    },
+    {
+        nome: 'Definição de Contexto',
+        grupo: 'PLAN'
+    },
+    {
+        nome: 'Análise/Avaliação dos riscos',
+        grupo: 'PLAN'
+    },
+    {
+        nome: 'Tratamento dos Riscos',
+        grupo: 'PLAN'
+    },
+    {
+        nome: 'Aceitação dos Riscos',
+        grupo: 'PLAN'
+    },
+    {
+        nome: 'Implementar o tratamento dos Riscos',
+        grupo: 'DO'
+    },
+    {
+        nome: 'Monitoração e revisão contínua',
+        grupo: 'CHECK'
+    },
+    {
+        nome: 'Manter e melhorar o processo de gestão de riscos',
+        grupo: 'ACT'
+    },
+
+];
+
+
+var grupos = [
+    {
+        nome: 'DEFINIÇÃO-CONTEXTO'    
+    },
+    {
+        nome: 'AVALIAÇÃO-DE-RISCOS-GERAL'    
+    },
+    {
+        nome: 'TRATAMENTO-DE-RISCOS'    
+    },
+    {
+        nome: 'ACEITAÇÃO-DE-RISCOS'    
+    },
+    {
+        nome: 'COMUNICAÇÃO-DE-RISCOS'    
+    },
+    {
+        nome: 'MONITORAÇÃO-REVISÃO'    
+    },
+    {
+        nome: 'PLAN'    
+    },
+    {
+        nome: 'DO'    
+    },
+    {
+        nome: 'ACT'    
+    },
+    {
+        nome: 'CHECK'    
+    },
+    {
+        nome: 'IDENTIFICAÇÃO-DE-RISCOS'    
+    },
+    {
+        nome: 'ESTIMATIVA-DE-RISCOS'    
+    },
+    {
+        nome: 'AVALIAÇÃO-DE-RISCOS'    
+    },
+    
+];
+
+
+var itensAcima = 6;

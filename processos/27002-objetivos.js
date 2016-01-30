50be2a7bdde3584a7a715949cea21c637e2b3a2d
Raphael Lacerda <lacerdaph@gmail.com>
2016-01-30 12:23:28 -0200

    objetivos 27002

---
+++ processos/27002-objetivos.js
@@ -0,0 +1,195 @@
+var processos = [
+    {
+        nome: 'Orientação da direção para SI',
+        grupo: '5-POLÍTICAS-DE-SEGURANÇA-INFORMAÇÃO'
+    },
+    {
+        nome: 'Organização Interna',
+        grupo: '6-ORGANIZAÇÃO-DA-SEGURANÇA-INFORMAÇÃO'
+    },
+    {
+        nome: 'Dispositivos móveis e trabalho remoto',
+        grupo: '6-ORGANIZAÇÃO-DA-SEGURANÇA-INFORMAÇÃO'
+    },
+    {
+        nome: 'Antes da contratação',
+        grupo: '7-SEGURANÇA-EM-RECURSOS-HUMANOS'
+    },
+    {
+        nome: 'Durante a contratação',
+        grupo: '7-SEGURANÇA-EM-RECURSOS-HUMANOS'
+    },
+    {
+        nome: 'Encerramento e mudança da contratação',
+        grupo: '7-SEGURANÇA-EM-RECURSOS-HUMANOS'
+    },
+    {
+        nome: 'Responsabilidade pelos ativos',
+        grupo: '8-GESTÃO-DE-ATIVOS'
+    },
+    {
+        nome: 'Classificação da Informação',
+        grupo: '8-GESTÃO-DE-ATIVOS'
+    },
+    {
+        nome: 'Tratamento de Mídias',
+        grupo: '8-GESTÃO-DE-ATIVOS'
+    },
+    {
+        nome: 'Responsabilidade pelos ativos',
+        grupo: '8-GESTÃO-DE-ATIVOS'
+    },
+    {
+        nome: 'Requisitos de Negócio para controle de acesso',
+        grupo: '9-CONTROLE-DE-ACESSO'
+    },
+    {
+        nome: 'Gerenciamento de acesso do usuário',
+        grupo: '9-CONTROLE-DE-ACESSO'
+    },
+    {
+        nome: 'Responsabilidade dos usuários',
+        grupo: '9-CONTROLE-DE-ACESSO'
+    },
+    {
+        nome: 'Controle de acesso ao sistema e à aplicação',
+        grupo: '9-CONTROLE-DE-ACESSO'
+    },
+    {
+        nome: 'Controles Criptográficos',
+        grupo: '10-CRIPTOGRAFIA'
+    },
+    {
+        nome: 'Áreas Seguras',
+        grupo: '11-SEGURANÇA-FÍSICA-E-DO-AMBIENTE'
+    },
+    {
+        nome: 'Equipamentos',
+        grupo: '11-SEGURANÇA-FÍSICA-E-DO-AMBIENTE'
+    },
+    {
+        nome: 'Responsabilidades e procedimentos operacionais',
+        grupo: '12-SEGURANÇA-NAS-OPERAÇÕES'
+    },
+    {
+        nome: 'Proteção contra códigos maliciosos',
+        grupo: '12-SEGURANÇA-NAS-OPERAÇÕES'
+    },
+    {
+        nome: 'Cópias de segurança',
+        grupo: '12-SEGURANÇA-NAS-OPERAÇÕES'
+    },
+    {
+        nome: 'Controle de software Operacional',
+        grupo: '12-SEGURANÇA-NAS-OPERAÇÕES'
+    },
+    {
+        nome: 'Gestão de vulnerabilidades técnicas',
+        grupo: '12-SEGURANÇA-NAS-OPERAÇÕES'
+    },
+    {
+        nome: 'Considerações quanto à auditoria de sistemas de informação',
+        grupo: '12-SEGURANÇA-NAS-OPERAÇÕES'
+    },
+    {
+        nome: 'Gerenciamento da segurança em redes',
+        grupo: '13-SEGURANÇA-NAS-COMUNICAÇÕES'
+    },
+    {
+        nome: 'Transferência de informação',
+        grupo: '13-SEGURANÇA-NAS-COMUNICAÇÕES'
+    },
+    {
+        nome: 'Requisitos de segurança de sistemas de informação',
+        grupo: '14-AQUISIÇÃO-DESENVOLVIMENTO-MANUTENÇÃO-SISTEMAS'
+    },
+    {
+        nome: 'Segurança em processos de desenvolvimento e de suporte',
+        grupo: '14-AQUISIÇÃO-DESENVOLVIMENTO-MANUTENÇÃO-SISTEMAS'
+    },
+    {
+        nome: 'Requisitos de segurança de sistemas de informação',
+        grupo: '14-AQUISIÇÃO-DESENVOLVIMENTO-MANUTENÇÃO-SISTEMAS'
+    },
+    {
+        nome: 'Dados para Teste',
+        grupo: '14-AQUISIÇÃO-DESENVOLVIMENTO-MANUTENÇÃO-SISTEMAS'
+    },
+    {
+        nome: 'Segurança da informação na cadeia de suprimento',
+        grupo: '15-RELAÇÃO-NA-CADEIA-DE-SUPRIMENTO'
+    },
+    {
+        nome: 'Gerenciamento da entrega do serviço do fornecedor',
+        grupo: '15-RELAÇÃO-NA-CADEIA-DE-SUPRIMENTO'
+    },
+    {
+        nome: 'Gestão de incidente de segurança da informação e melhorias',
+        grupo: '16-GESTÃO-INCIDENTES-DE-SEGURANÇA-INFORMAÇÃO'
+    },
+    {
+        nome: 'Continuidade da segurança da informação',
+        grupo: '17-ASPECTOS-DA-SI-NA-GESTÃO-DA-CONTINUIDADE-DO-NEGÓCIO'
+    },
+    {
+        nome: 'Redundâncias',
+        grupo: '17-ASPECTOS-DA-SI-NA-GESTÃO-DA-CONTINUIDADE-DO-NEGÓCIO'
+    },
+    {
+        nome: 'Conformidade com os requisistos legais',
+        grupo: '18-CONFORMIDADE'
+    },
+    {
+        nome: 'Análise crítica da segurança da informação',
+        grupo: '18-CONFORMIDADE'
+    }
+];
+
+
+var grupos = [
+    {
+        nome: '5-POLÍTICAS-DE-SEGURANÇA-INFORMAÇÃO'    
+    },
+    {
+        nome: '6-ORGANIZAÇÃO-DA-SEGURANÇA-INFORMAÇÃO'    
+    },
+    {
+        nome: '7-SEGURANÇA-EM-RECURSOS-HUMANOS'    
+    },
+    {
+        nome: '8-GESTÃO-DE-ATIVOS'    
+    },
+    {
+        nome: '9-CONTROLE-DE-ACESSO'    
+    },
+    {
+        nome: '10-CRIPTOGRAFIA'    
+    },
+    {
+        nome: '11-SEGURANÇA-FÍSICA-E-DO-AMBIENTE'    
+    },
+    {
+        nome: '12-SEGURANÇA-NAS-OPERAÇÕES'    
+    },
+    {
+        nome: '13-SEGURANÇA-NAS-COMUNICAÇÕES'    
+    },
+    {
+        nome: '14-AQUISIÇÃO-DESENVOLVIMENTO-MANUTENÇÃO-SISTEMAS'    
+    },
+    {
+        nome: '15-RELAÇÃO-NA-CADEIA-DE-SUPRIMENTO'    
+    },
+    {
+        nome: '16-GESTÃO-INCIDENTES-DE-SEGURANÇA-INFORMAÇÃO'    
+    },
+    {
+        nome: '17-ASPECTOS-DA-SI-NA-GESTÃO-DA-CONTINUIDADE-DO-NEGÓCIO'    
+    },
+    {
+        nome: '18-CONFORMIDADE'    
+    }
+];
+
+
+var itensAcima = 6;

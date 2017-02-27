af8b3c127e7f987e5331faca31bd4e441f65cc4c
Raphael Lacerda <lacerdaph@gmail.com>
2017-02-27 13:49:17 -0300

    ajustes no cmmi

--- js/drag.js
+++ js/drag.js
@@ -4,121 +4,121 @@ var mapaDeZonaPorProcessos = [];
 function dropzone() {
 
 
-  /* The dragging code for '.draggable' from the demo above
-   * applies to this demo as well so it doesn't have to be repeated. */
+    /* The dragging code for '.draggable' from the demo above
+     * applies to this demo as well so it doesn't have to be repeated. */
 
-  // enable draggables to be dropped into this
-  interact('.dropzone').dropzone({
-    // only accept elements matching this CSS selector
-    accept: '.draggable',
-    // Require a 75% element overlap for a drop to be possible
-    //overlap: 0.25,
+    // enable draggables to be dropped into this
+    interact('.dropzone').dropzone({
+        // only accept elements matching this CSS selector
+        accept: '.draggable',
+        // Require a 75% element overlap for a drop to be possible
+        //overlap: 0.25,
 
-    // listen for drop related events:
+        // listen for drop related events:
 
-    ondropactivate: function (event) {
-      // add active dropzone feedback
-      event.target.classList.add('drop-active');
-    },
-    ondragenter: function (event) {
-      var draggableElement = event.relatedTarget,
-        dropzoneElement = event.target;
+        ondropactivate: function(event) {
+            // add active dropzone feedback
+            event.target.classList.add('drop-active');
+        },
+        ondragenter: function(event) {
+            var draggableElement = event.relatedTarget,
+                dropzoneElement = event.target;
 
-      // feedback the possibility of a drop
-      dropzoneElement.classList.add('drop-target');
-      draggableElement.classList.add('can-drop');
-      //draggableElement.textContent = 'Dragged in';
-    },
-    ondragleave: function (event) {
-      // remove the drop feedback style
-      event.target.classList.remove('drop-target');
-      event.relatedTarget.classList.remove('can-drop');
-      event.relatedTarget.classList.remove('error');
-      //event.relatedTarget.textContent = 'Dragged out';
-      var id = event.relatedTarget.id;
-      
-      //$('#' +id).removeAttr('title');
-      
-      var nomeDaZona = event.target.id;
+            // feedback the possibility of a drop
+            dropzoneElement.classList.add('drop-target');
+            draggableElement.classList.add('can-drop');
+            //draggableElement.textContent = 'Dragged in';
+        },
+        ondragleave: function(event) {
+            // remove the drop feedback style
+            event.target.classList.remove('drop-target');
+            event.relatedTarget.classList.remove('can-drop');
+            event.relatedTarget.classList.remove('error');
+            //event.relatedTarget.textContent = 'Dragged out';
+            var id = event.relatedTarget.id;
 
-      var zona = _.find(mapaDeZonaPorProcessos, function (z) {
-        return z.zona === nomeDaZona;
-      });
+            //$('#' +id).removeAttr('title');
 
-      if (zona && zona.processos) {
+            var nomeDaZona = event.target.id;
 
-          
-        console.log('tamanho do array: ' + zona.processos.length);
+            var zona = _.find(mapaDeZonaPorProcessos, function(z) {
+                return z.zona === nomeDaZona;
+            });
 
-        zona.processos = _.reject(zona.processos, function (p) {
-          
-            //retirar o processo da zona
-            
-            
-          return p.id == id;
-        });
-
-        console.log('novo tamanho do array: ' + zona.processos.length);
-        event.target.textContent = nomeDaZona + ' - ' + zona.processos.length;
-
-      }
+            if (zona && zona.processos) {
 
 
-    },
+                console.log('tamanho do array: ' + zona.processos.length);
 
-    ondrop: function (event) {
+                zona.processos = _.reject(zona.processos, function(p) {
 
-      var id = event.relatedTarget.id;
+                    //retirar o processo da zona
 
-      var nomeDaZona = event.target.id;
 
-      var processoDropado = _.find(processos, function (p) {
-        //aqui a comparação é com ==
-        //eu quero que ele faça o casting
-        return p.id == id;
-      });
+                    return p.id == id;
+                });
 
-      //var jsonString = $('#' + id).attr('json');
-      //var processo = eval("(" + jsonString + ')');
+                console.log('novo tamanho do array: ' + zona.processos.length);
+                event.target.textContent = nomeDaZona + ' - ' + zona.processos.length;
 
-      var zona = _.find(mapaDeZonaPorProcessos, function (z) {
-        return z.zona === nomeDaZona;
-      });
+            }
 
-      if (!zona) {
 
-        console.log('zona iniciada');
+        },
 
-        mapaDeZonaPorProcessos.push({
+        ondrop: function(event) {
 
-          zona: nomeDaZona,
-          processos: [processoDropado]
+            var id = event.relatedTarget.id;
 
-        });
+            var nomeDaZona = event.target.id;
 
-        event.target.textContent = nomeDaZona + " - " + 1;
-      } else {
+            var processoDropado = _.find(processos, function(p) {
+                //aqui a comparação é com ==
+                //eu quero que ele faça o casting
+                return p.id == id;
+            });
 
-        var processoEncontrado = _.find(zona.processos, function (p) {
-          return p.id === processoDropado.id
-        });
+            //var jsonString = $('#' + id).attr('json');
+            //var processo = eval("(" + jsonString + ')');
 
-        if (!processoEncontrado) {
-          zona.processos.push(processoDropado);
-          event.target.agrupados++;
-        } else {
-          console.log('elemento já existe na zona: ' + JSON.stringify(processoDropado));
+            var zona = _.find(mapaDeZonaPorProcessos, function(z) {
+                return z.zona === nomeDaZona;
+            });
+
+            if (!zona) {
+
+                console.log('zona iniciada');
+
+                mapaDeZonaPorProcessos.push({
+
+                    zona: nomeDaZona,
+                    processos: [processoDropado]
+
+                });
+
+                event.target.textContent = nomeDaZona + " - " + 1;
+            } else {
+
+                var processoEncontrado = _.find(zona.processos, function(p) {
+                    return p.id === processoDropado.id
+                });
+
+                if (!processoEncontrado) {
+                    zona.processos.push(processoDropado);
+                    event.target.agrupados++;
+                } else {
+                    console.log('elemento já existe na zona: ' + JSON.stringify(processoDropado));
+                }
+                event.target.textContent = nomeDaZona + " - " + zona.processos.length;
+            }
+
+        },
+        ondropdeactivate: function(event) {
+            // remove active dropzone feedback
+            event.target.classList.remove('drop-active');
+            event.target.classList.remove('drop-target');
         }
-        event.target.textContent = nomeDaZona + " - " + zona.processos.length;
-      }
-
-    },
-    ondropdeactivate: function (event) {
-      // remove active dropzone feedback
-      event.target.classList.remove('drop-active');
-      event.target.classList.remove('drop-target');
-    }
-  });
+    });
 
 
 }
@@ -127,50 +127,50 @@ function dropzone() {
 
 function draggable() {
 
-  interact('.draggable')
-    .draggable({
-      // enable inertial throwing
-      inertia: true,
-      // keep the element within the area of it's parent
-      restrict: {
-        //restriction: "parent",
-        endOnly: true,
-        elementRect: {
-          top: 0,
-          left: 0,
-          bottom: 1,
-          right: 1
-        }
-      },
+    interact('.draggable')
+        .draggable({
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                //restriction: "parent",
+                endOnly: true,
+                elementRect: {
+                    top: 0,
+                    left: 0,
+                    bottom: 1,
+                    right: 1
+                }
+            },
 
-      // call this function on every dragmove event
-      onmove: dragMoveListener,
-      // call this function on every dragend event
-      onend: function (event) {
-        var textEl = event.target.querySelector('p');
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            // call this function on every dragend event
+            onend: function(event) {
+                var textEl = event.target.querySelector('p');
 
-        textEl && (textEl.textContent =
-          'moved a distance of ' + (Math.sqrt(event.dx * event.dx +
-            event.dy * event.dy) | 0) + 'px');
-      }
-    });
+                textEl && (textEl.textContent =
+                    'moved a distance of ' + (Math.sqrt(event.dx * event.dx +
+                        event.dy * event.dy) | 0) + 'px');
+            }
+        });
 }
 
 
 function dragMoveListener(event) {
-  var target = event.target,
-    // keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-  // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-    'translate(' + x + 'px, ' + y + 'px)';
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
 
-  // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
 }
 
 // this is used later in the resizing demo
--- processos/cmmi-processos.js
+++ processos/cmmi-processos.js
@@ -1,107 +1,105 @@
-var processos = [
-    {
+var processos = [{
         nome: 'Gerência de Requisitos',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Gerência de Projetos',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Monitoramento e Controle de Projeto',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Gerência de Contrato com Fornecedores',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Gerência de Configuração',
         grupo: 'SUPORTE'
     },
-	{
+    {
         nome: 'Garantia de Qualidade de Processo e Produto',
         grupo: 'SUPORTE'
     },
-	{
+    {
         nome: 'Medição e Análise',
         grupo: 'SUPORTE'
     },
-	{
+    {
         nome: 'Foco no Processo da Organização',
         grupo: 'PROCESSO'
     },
-   {
+    {
         nome: 'Definição do Processo da Organização',
         grupo: 'PROCESSO'
     },
-	{
+    {
         nome: 'Treinamento Organizacional',
         grupo: 'PROCESSO'
     },
-	{
+    {
         nome: 'Gerência de Projeto Integrada',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Gerência de Riscos',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Desenvolvimento de Requisitos',
         grupo: 'ENGENHARIA'
     },
-	{
+    {
         nome: 'Solução Técnica',
         grupo: 'ENGENHARIA'
     },
-	{
+    {
         nome: 'Integração de Produto',
         grupo: 'ENGENHARIA'
     },
-	{
+    {
         nome: 'Verificação',
         grupo: 'ENGENHARIA'
     },
-	{
+    {
         nome: 'Validação',
         grupo: 'ENGENHARIA'
     },
-	{
+    {
         nome: 'Análise e Tomada de Decisões',
         grupo: 'SUPORTE'
     },
-	{
+    {
         nome: 'Gestão Quantitativa de Projeto',
         grupo: 'PROJETO'
     },
-	{
+    {
         nome: 'Desempenho dos processos da Organização',
         grupo: 'PROCESSO'
     },
-	{
+    {
         nome: 'Análise Causal e Resolução',
         grupo: 'SUPORTE'
     },
-	{
+    {
         nome: 'Inovação e Melhoria Organizacional',
         grupo: 'PROCESSO'
     }
 ];
 
 
-var grupos = [
-    {
-        nome: 'PROJETO'    
+var grupos = [{
+        nome: 'PROJETO'
     },
     {
-        nome: 'PROCESSO'    
+        nome: 'PROCESSO'
     },
     {
-        nome: 'ENGENHARIA'    
+        nome: 'ENGENHARIA'
     },
     {
-        nome: 'SUPORTE'    
+        nome: 'SUPORTE'
     }
 ];
 

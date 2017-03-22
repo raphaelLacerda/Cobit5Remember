b30df9309a8a82d6ca32aa7c1c78bca7df4bffdb
Raphael Lacerda <lacerdaph@gmail.com>
2017-03-22 14:25:11 -0300

    COBIT 5 - capacidade e fases

---
+++ processos/cobit5-fases.js
@@ -0,0 +1,100 @@
+var processos = [
+    {
+    nome: 'Reconhecer ações necessárias para agir',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Ativos',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Definir Objetivo alvo',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Construir Melhorias',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Implementar Melhorias',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Operar e Medir',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Monitorar e Avaliar',
+    grupo: 'Ciclo de Vida'
+},
+{
+    nome: 'Estabelecer desejo por mudança',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Formação da equipe de implementação',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Comunicação',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Identificar papéis',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Operação e uso',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Receber novas formas',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Sustentar',
+    grupo: 'Mudança'
+},
+{
+    nome: 'Inicializar',
+    grupo: 'Programa'
+},
+{
+    nome: 'Definir Problemas e Oportunidades',
+    grupo: 'Programa'
+},
+{
+    nome: 'Road map',
+    grupo: 'Programa'
+},
+{
+    nome: 'Planejar',
+    grupo: 'Programa'
+},
+{
+    nome: 'Executar',
+    grupo: 'Programa'
+},
+{
+    nome: 'Obter Benefícios',
+    grupo: 'Programa'
+},
+{
+    nome: 'Rever Efetividade',
+    grupo: 'Programa'
+},
+];
+
+
+
+var grupos = [{
+        nome: 'Ciclo de Vida'
+    }, {
+        nome: 'Programa'
+    }, {
+        nome: 'Mudança'
+    }
+
+];
+
+var itensAcima = 2;
--- processos/cobit5.js
+++ processos/cobit5.js
@@ -1,310 +1,231 @@
-var processos = [
-    {
-        nome: 'Reunir necessidades do StakeHolders',
-        grupo: 'Principios'
-    },
-    {
-        nome: 'Cobrir a empresa fim a fim',
-        grupo: 'Principios'
-    },
-    {
-        nome: 'Aplicar um Framework único integrado',
-        grupo: 'Principios'
-    },
-    {
-        nome: 'Abordagem Holística',
-        grupo: 'Principios'
-    },
-    {
-        nome: 'Separar governança de gestão',
-        grupo: 'Principios'
-    },
-    {
-        nome: 'Princípios, políticas e frameworks',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Processos',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Pessoas, Habilitadores, competências',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Estruturas Organizacionais',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Cultura, Ética e Comportamento',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Informação',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Serviços / Infraestrutura / Aplicações',
-        grupo: 'Categorias'
-        },
-    {
-        nome: 'Partes Interessadas',
-        grupo: 'Dimensoes'
-        },
-    {
-        nome: 'Metas',
-        grupo: 'Dimensoes'
-        },
-    {
-        nome: 'Ciclo de Vida',
-        grupo: 'Dimensoes'
-        },
-    {
-        nome: 'Boas Práticas',
-        grupo: 'Dimensoes'
-        },
-    {
-        nome: 'Stakeholder drivers',
-        grupo: 'Objetivos'
-        },
-    {
-        nome: 'Stakeholder needs',
-        grupo: 'Objetivos'
-        },
-    {
-        nome: 'Objetivos genéricos empresariais / corporativos',
-        grupo: 'Objetivos'
-        },
-    {
-        nome: 'Objetivos relacionados a TI',
-        grupo: 'Objetivos'
-        },
-    {
-        nome: 'Objetivo dos facilitadores',
-        grupo: 'Objetivos'
-        },
-    {
-        nome: 'Avaliar',
-        grupo: 'Governança'
-        },
-    {
-        nome: 'Dirigir',
-        grupo: 'Governança'
-        },
-    {
-        nome: 'Monitorar a decisões',
-        grupo: 'Governança'
-        },
-    {
-        nome: 'Responsabilidade é da alta administração',
-        grupo: 'Governança'
-        },
-    {
-        nome: 'assegurar necessidades dos stakeholders',
-        grupo: 'Governança'
-        },
-    {
-        nome: 'Planejar',
-        grupo: 'Gestão'
-        },
-    {
-        nome: 'Construir',
-        grupo: 'Gestão'
-        },
-    {
-        nome: 'Executar',
-        grupo: 'Gestão'
-        },
-    {
-        nome: 'Monitorar as atividades',
-        grupo: 'Gestão'
-        },
-    {
-        nome: 'Garantir o framework de Governança e sua manutenção',
-        grupo: 'Avaliar-Direcionar-Monitorar'
-        },
-    {
-        nome: 'Garantir os benefícios de entrega',
-        grupo: 'Avaliar-Direcionar-Monitorar'
-        },
-    {
-        nome: 'Garantir a otimização dos riscos e recursos',
-        grupo: 'Avaliar-Direcionar-Monitorar'
-        },
-    {
-        nome: 'Garantir a otimização dos recursos e custos',
-        grupo: 'Avaliar-Direcionar-Monitorar'
-        },
-    {
-        nome: 'Garantir transparência aos stakeholders',
-        grupo: 'Avaliar-Direcionar-Monitorar'
-        },
-    {
-        nome: 'Gerenciar o framework de gerenciamento de TI',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar estratégia',
-        grupo: 'Alinhar-Planejar-Organizar'
-        },
-    {
-        nome: 'Gerenciar Arquitetura Empresarial',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Inovação',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Portfólio',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar orçamento e custos',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Recursos Humanos',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Relacionamentos',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar acordos de serviço',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Fornecedores',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Qualidade',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Riscos',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar Segurança',
-        grupo: 'Alinhar-Planejar-Organizar'
-        }, 
-    {
-        nome: 'Gerenciar programas e projetos',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar definição de requisitos',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar soluções e construções',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar a Disponibilidade e Capacidade ',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar a implementação de Mudança Organizacional',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar Mudanças',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar Aceite e Transição de Mudança',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar o Conhecimento ',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar os Ativos',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar a Configuração',
-        grupo: 'Construir-Adquirir-Implementar'
-        }, 
-    {
-        nome: 'Gerenciar as Operações',
-        grupo: 'Entregar-Servir-Suportar'
-        }, 
-    {
-        nome: 'Gerenciar Requisições de Serviço e Incidentes',
-        grupo: 'Entregar-Servir-Suportar'
-        }, 
-    {
-        nome: 'Gerenciar Problemas',
-        grupo: 'Entregar-Servir-Suportar'
-        }, 
-    {
-        nome: 'Gerenciar a Continuidade',
-        grupo: 'Entregar-Servir-Suportar'
-        }, 
-    {
-        nome: 'Gerenciar os Serviços de Segurança',
-        grupo: 'Entregar-Servir-Suportar'
-        }, 
-    {
-        nome: 'Gerenciar os Controles de Processos de Negocio',
-        grupo: 'Entregar-Servir-Suportar'
-        }, 
-    {
-        nome: 'Monitorar, Avaliar e Medir o Desempenho e Conformidade',
-        grupo: 'Monitorar-Avaliar-Medir'
-        }, 
-    {
-        nome: 'Controle Interno',
-        grupo: 'Monitorar-Avaliar-Medir'
-        }, 
-    {
-        nome: 'Requisitos Externos',
-        grupo: 'Monitorar-Avaliar-Medir'
-        }
-    ];
+var processos = [{
+    nome: 'Reunir necessidades do StakeHolders',
+    grupo: 'Principios'
+}, {
+    nome: 'Cobrir a empresa fim a fim',
+    grupo: 'Principios'
+}, {
+    nome: 'Aplicar um Framework único integrado',
+    grupo: 'Principios'
+}, {
+    nome: 'Abordagem Holística',
+    grupo: 'Principios'
+}, {
+    nome: 'Separar governança de gestão',
+    grupo: 'Principios'
+}, {
+    nome: 'Princípios, políticas e frameworks',
+    grupo: 'Categorias'
+}, {
+    nome: 'Processos',
+    grupo: 'Categorias'
+}, {
+    nome: 'Pessoas, Habilitadores, competências',
+    grupo: 'Categorias'
+}, {
+    nome: 'Estruturas Organizacionais',
+    grupo: 'Categorias'
+}, {
+    nome: 'Cultura, Ética e Comportamento',
+    grupo: 'Categorias'
+}, {
+    nome: 'Informação',
+    grupo: 'Categorias'
+}, {
+    nome: 'Serviços / Infraestrutura / Aplicações',
+    grupo: 'Categorias'
+}, {
+    nome: 'Partes Interessadas',
+    grupo: 'Dimensoes'
+}, {
+    nome: 'Metas',
+    grupo: 'Dimensoes'
+}, {
+    nome: 'Ciclo de Vida',
+    grupo: 'Dimensoes'
+}, {
+    nome: 'Boas Práticas',
+    grupo: 'Dimensoes'
+}, {
+    nome: 'Stakeholder drivers',
+    grupo: 'Objetivos'
+}, {
+    nome: 'Stakeholder needs',
+    grupo: 'Objetivos'
+}, {
+    nome: 'Objetivos genéricos empresariais / corporativos',
+    grupo: 'Objetivos'
+}, {
+    nome: 'Objetivos relacionados a TI',
+    grupo: 'Objetivos'
+}, {
+    nome: 'Objetivo dos facilitadores',
+    grupo: 'Objetivos'
+}, {
+    nome: 'Avaliar',
+    grupo: 'Governança'
+}, {
+    nome: 'Dirigir',
+    grupo: 'Governança'
+}, {
+    nome: 'Monitorar a decisões',
+    grupo: 'Governança'
+}, {
+    nome: 'Responsabilidade é da alta administração',
+    grupo: 'Governança'
+}, {
+    nome: 'assegurar necessidades dos stakeholders',
+    grupo: 'Governança'
+}, {
+    nome: 'Planejar',
+    grupo: 'Gestão'
+}, {
+    nome: 'Construir',
+    grupo: 'Gestão'
+}, {
+    nome: 'Executar',
+    grupo: 'Gestão'
+}, {
+    nome: 'Monitorar as atividades',
+    grupo: 'Gestão'
+}, {
+    nome: 'Garantir o framework de Governança e sua manutenção',
+    grupo: 'Avaliar-Direcionar-Monitorar'
+}, {
+    nome: 'Garantir os benefícios de entrega',
+    grupo: 'Avaliar-Direcionar-Monitorar'
+}, {
+    nome: 'Garantir a otimização dos riscos e recursos',
+    grupo: 'Avaliar-Direcionar-Monitorar'
+}, {
+    nome: 'Garantir a otimização dos recursos e custos',
+    grupo: 'Avaliar-Direcionar-Monitorar'
+}, {
+    nome: 'Garantir transparência aos stakeholders',
+    grupo: 'Avaliar-Direcionar-Monitorar'
+}, {
+    nome: 'Gerenciar o framework de gerenciamento de TI',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar estratégia',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Arquitetura Empresarial',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Inovação',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Portfólio',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar orçamento e custos',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Recursos Humanos',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Relacionamentos',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar acordos de serviço',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Fornecedores',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Qualidade',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Riscos',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar Segurança',
+    grupo: 'Alinhar-Planejar-Organizar'
+}, {
+    nome: 'Gerenciar programas e projetos',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar definição de requisitos',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar soluções e construções',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar a Disponibilidade e Capacidade ',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar a implementação de Mudança Organizacional',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar Mudanças',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar Aceite e Transição de Mudança',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar o Conhecimento ',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar os Ativos',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar a Configuração',
+    grupo: 'Construir-Adquirir-Implementar'
+}, {
+    nome: 'Gerenciar as Operações',
+    grupo: 'Entregar-Servir-Suportar'
+}, {
+    nome: 'Gerenciar Requisições de Serviço e Incidentes',
+    grupo: 'Entregar-Servir-Suportar'
+}, {
+    nome: 'Gerenciar Problemas',
+    grupo: 'Entregar-Servir-Suportar'
+}, {
+    nome: 'Gerenciar a Continuidade',
+    grupo: 'Entregar-Servir-Suportar'
+}, {
+    nome: 'Gerenciar os Serviços de Segurança',
+    grupo: 'Entregar-Servir-Suportar'
+}, {
+    nome: 'Gerenciar os Controles de Processos de Negocio',
+    grupo: 'Entregar-Servir-Suportar'
+}, {
+    nome: 'Monitorar, Avaliar e Medir o Desempenho e Conformidade',
+    grupo: 'Monitorar-Avaliar-Medir'
+}, {
+    nome: 'Controle Interno',
+    grupo: 'Monitorar-Avaliar-Medir'
+}, {
+    nome: 'Requisitos Externos',
+    grupo: 'Monitorar-Avaliar-Medir'
+}];
 
 
-var grupos = [
-    {
-        nome: 'Principios'    
-    },
-    {
-        nome: 'Categorias'    
-    },
-    {
-        nome: 'Dimensoes'    
-    },
-    {
-        nome: 'Objetivos'    
-    },
-    {
-        nome: 'Governança'    
-    },
-    {
-        nome: 'Gestão'    
-    },
-    {
-        nome: 'Avaliar-Direcionar-Monitorar'    
-    },
-    {
-        nome: 'Alinhar-Planejar-Organizar'    
-    },
-    {
-        nome: 'Construir-Adquirir-Implementar'    
-    },
-    {
-        nome: 'Entregar-Servir-Suportar'    
-    },
-    {
-        nome: 'Monitorar-Avaliar-Medir'    
+var grupos = [{
+        nome: 'Principios'
+    }, {
+        nome: 'Categorias'
+    }, {
+        nome: 'Dimensoes'
+    }, {
+        nome: 'Objetivos'
+    }, {
+        nome: 'Governança'
+    }, {
+        nome: 'Gestão'
+    }, {
+        nome: 'Avaliar-Direcionar-Monitorar'
+    }, {
+        nome: 'Alinhar-Planejar-Organizar'
+    }, {
+        nome: 'Construir-Adquirir-Implementar'
+    }, {
+        nome: 'Entregar-Servir-Suportar'
+    }, {
+        nome: 'Monitorar-Avaliar-Medir'
     }
-    
+
 ];
 
 var itensAcima = 6;

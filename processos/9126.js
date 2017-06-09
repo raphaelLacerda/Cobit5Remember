198af0c9738ddf87d6ba865450ce8eb2d5fed1fd
Raphael Lacerda <lacerdaph@gmail.com>
2017-06-09 01:13:23 -0300

    iso 9126

---
+++ processos/9126.js
@@ -0,0 +1,152 @@
+var processos = [{
+        nome: 'Adequação',
+        grupo: 'FUNCIONALIDADE'
+    },
+    {
+        nome: 'Acurácia',
+        grupo: 'FUNCIONALIDADE'
+    },
+    {
+        nome: 'Interoperabilidade',
+        grupo: 'FUNCIONALIDADE'
+    },
+    {
+        nome: 'Segurança de acesso',
+        grupo: 'FUNCIONALIDADE'
+    },
+    {
+        nome: 'Conformidade da funcionalidade',
+        grupo: 'FUNCIONALIDADE'
+    },
+    {
+        nome: 'Maturidade',
+        grupo: 'CONFIABILIDADE'
+    },
+    {
+        nome: 'Tolerância a falhas',
+        grupo: 'CONFIABILIDADE'
+    },
+    {
+        nome: 'Recuperabilidade',
+        grupo: 'CONFIABILIDADE'
+    },
+    {
+        nome: 'Conformidade relacionada à confiabilidade',
+        grupo: 'CONFIABILIDADE'
+    },
+    {
+        nome: 'Inteligibilidade',
+        grupo: 'USABILIDADE'
+    },
+    {
+        nome: 'Apreensibilidade',
+        grupo: 'USABILIDADE'
+    },
+    {
+        nome: 'Operacionalidade',
+        grupo: 'USABILIDADE'
+    },
+    {
+        nome: 'Atratividade',
+        grupo: 'USABILIDADE'
+    },
+    {
+        nome: 'Conformidade relacionada à usabilidade',
+        grupo: 'USABILIDADE'
+    },
+    {
+        nome: 'Comportamento com relação ao tempo',
+        grupo: 'EFICIENCIA'
+    },
+    {
+        nome: 'Utilização de recursos',
+        grupo: 'EFICIENCIA'
+    },
+    {
+        nome: 'Conformidade relacionada à eficiência',
+        grupo: 'EFICIENCIA'
+    },
+    {
+        nome: 'Analisabilidade',
+        grupo: 'MANUTENIBILIDADE'
+    },
+    {
+        nome: 'Modificabilidade',
+        grupo: 'MANUTENIBILIDADE'
+    },
+    {
+        nome: 'Estabilidade',
+        grupo: 'MANUTENIBILIDADE'
+    },
+    {
+        nome: 'Testabilidade',
+        grupo: 'MANUTENIBILIDADE'
+    },
+    {
+        nome: 'Conformidade relacionada à manutenibilidade',
+        grupo: 'MANUTENIBILIDADE'
+    },
+    {
+        nome: 'Adaptabilidade',
+        grupo: 'PORTABILIDADE'
+    },
+    {
+        nome: 'Capacidade para ser instalado',
+        grupo: 'PORTABILIDADE'
+    },
+    {
+        nome: 'Coexistência',
+        grupo: 'PORTABILIDADE'
+    },
+    {
+        nome: 'Capacidade para substituir',
+        grupo: 'PORTABILIDADE'
+    },
+    {
+        nome: 'Conformidade relacionada à portabilidade',
+        grupo: 'PORTABILIDADE'
+    },
+    {
+        nome: 'Segurança',
+        grupo: 'QUALIDADE-EM-USO'
+    },
+    {
+        nome: 'Eficácia',
+        grupo: 'QUALIDADE-EM-USO'
+    },
+    {
+        nome: 'Satisfação',
+        grupo: 'QUALIDADE-EM-USO'
+    },
+    {
+        nome: 'Produtividade',
+        grupo: 'QUALIDADE-EM-USO'
+    },
+];
+
+
+var grupos = [{
+        nome: 'CONFIABILIDADE'
+    },
+    {
+        nome: 'EFICIENCIA'
+    },
+    {
+        nome: 'FUNCIONALIDADE'
+    },
+    {
+        nome: 'MANUTENIBILIDADE'
+    },
+    {
+        nome: 'PORTABILIDADE'
+    },
+    {
+        nome: 'USABILIDADE'
+    },
+    {
+        nome: 'QUALIDADE-EM-USO'
+    }
+];
+
+
+var itensAcima = 3;

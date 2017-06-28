b2861601b3bb36b4c78c794442a8b256db96dacd
Raphael Lacerda <lacerdaph@gmail.com>
2017-06-28 13:04:49 -0300

    escritorios de projet - pmbok

---
+++ processos/pmbok-escritorios.js
@@ -0,0 +1,119 @@
+var processos = [{
+        nome: 'Papel consultivo',
+        grupo: 'Suporte'
+    }, {
+        nome: 'fornecer modelos',
+        grupo: 'Suporte'
+    },{
+        nome: 'treinamento',
+        grupo: 'Suporte'
+    },{
+        nome: 'melhores práticas',
+        grupo: 'Suporte'
+    },
+    {
+        nome: 'acesso a informações',
+        grupo: 'Suporte'
+    },
+    {
+        nome: 'lições aprendidas',
+        grupo: 'Suporte'
+    },
+    {
+        nome: 'repositório de projetos',
+        grupo: 'Suporte'
+    },
+    {
+        nome: 'controle baixo',
+        grupo: 'Suporte'
+    },
+    {
+        nome: 'exigir conformidade',
+        grupo: 'Controle'
+    },{
+        nome: 'adoção de estruturas e metodologias',
+        grupo: 'Controle'
+    },
+    {
+        nome: 'controle médio',
+        grupo: 'Controle'
+    },
+    {
+        nome: 'assumem o controle',
+        grupo: 'Diretivo'
+    },
+    {
+        nome: 'gerenciamento direto',
+        grupo: 'Diretivo'
+    },
+    {
+        nome: 'controle alto',
+        grupo: 'Diretivo'
+    },
+    {
+        nome: 'gestão corporativa de recursos',
+        grupo: 'Diretivo'
+    },
+    {
+        nome: 'conhecido como PMO de nível estratégico',
+        grupo: 'Diretivo'
+    },
+
+
+
+
+
+    {
+        nome: 'conhecimento, desempenho, pessoal',
+        grupo: 'Gerente-Projeto'
+    },
+    {
+        nome: 'se concentra nos objetivos especificados do projeto',
+        grupo: 'Gerente-Projeto'
+    },
+    {
+        nome: 'controla recursos alocados',
+        grupo: 'Gerente-Projeto'
+    },
+    {
+        nome: 'gerencia restrições',
+        grupo: 'Gerente-Projeto'
+    },
+    {
+        nome: 'gerencia as principais mudanas do escopo do programa',
+        grupo: 'PMO'
+    },
+    {
+        nome: 'otimiza o uso de recursos organizacionais compartilhados',
+        grupo: 'PMO'
+    },
+    {
+        nome: 'gerencia metodologias, padrões, riscos e oportunidades, métricas',
+        grupo: 'PMO'
+    },
+    {
+        nome: 'gerencia interdependencia entre os projetos',
+        grupo: 'PMO'
+    },
+    {
+        nome: 'avalia como os objetivos estratégicos de nível mais alto estão sendo alcançados',
+        grupo: 'PMO'
+    },
+
+];
+
+
+
+var grupos = [{
+    nome: 'Suporte'
+}, {
+    nome: 'Controle'
+}, {
+    nome: 'Diretivo'
+}, {
+    nome: 'Gerente-Projeto'
+}, {
+    nome: 'PMO'
+}];
+
+var itensAcima = 3;

6c1f9c7b9767eaced0e8ec9a08bdb47dc15ae89a
Raphael Lacerda <lacerdaph@gmail.com>
2017-07-12 20:33:18 -0300

    bpm

---
+++ processos/bpm.js
@@ -0,0 +1,81 @@
+var processos = [{
+        nome: 'Eventos',
+        grupo: 'FLOW'
+    },
+    {
+        nome: 'Atividades',
+        grupo: 'FLOW'
+    },
+    {
+        nome: 'Gateways',
+        grupo: 'FLOW'
+    },
+    {
+        nome: 'Objeto de dados',
+        grupo: 'DATA'
+    },
+    {
+        nome: 'Dados de Entrada',
+        grupo: 'DATA'
+    },
+    {
+        nome: 'Dados de saída',
+        grupo: 'DATA'
+    },
+    {
+        nome: 'Armazenamento',
+        grupo: 'DATA'
+    },
+    {
+        nome: 'Fluxo de Sequência',
+        grupo: 'CONNECTING-OBJECTS'
+    },
+    {
+        nome: 'Fluxo de Mensagem',
+        grupo: 'CONNECTING-OBJECTS'
+    },
+    {
+        nome: 'Associação',
+        grupo: 'CONNECTING-OBJECTS'
+    },
+    {
+        nome: 'Associação de dados',
+        grupo: 'CONNECTING-OBJECTS'
+    },
+
+    {
+        nome: 'Piscinas',
+        grupo: 'SWINLANES'
+    },
+
+    {
+        nome: 'Raias',
+        grupo: 'SWINLANES'
+    },
+
+    {
+        nome: 'Grupos',
+        grupo: 'ARTIFACTS'
+    },
+    {
+        nome: 'Anotações de Texto',
+        grupo: 'ARTIFACTS'
+    },
+
+];
+
+
+
+var grupos = [{
+    nome: 'FLOW'
+}, {
+    nome: 'DATA'
+}, {
+    nome: 'CONNECTING-OBJECTS'
+}, {
+    nome: 'SWINLANES'
+}, {
+    nome: 'ARTIFACTS'
+}];
+
+var itensAcima = 3;

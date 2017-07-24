649a5563838cb8300de335af50db91c6df9be933
Raphael Lacerda <lacerdaph@gmail.com>
2017-07-23 22:50:05 -0300

    mpsbr - componente referencia de servicos

--- processos/mpsbr-niveis.js
+++ processos/mpsbr-niveis.js
@@ -1,33 +1,60 @@
-var processos = [
-    {
+var processos = [{
         nome: 'Gerência de Requisitos',
         grupo: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
     },
-	{
+    {
         nome: 'Gerência de Projetos',
         grupo: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
     },
-	{
+    {
+        nome: 'Gerência de Incidente',
+        grupo: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
+    },
+    {
+        nome: 'Gerência de Solicitação de Serviços',
+        grupo: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
+    },
+    {
+        nome: 'Gerência de Nível de Serviço',
+        grupo: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
+    },
+    {
+        nome: 'Gerência de Operação de Serviço',
+        grupo: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
+    },
+    {
+        nome: 'Gerência de Problemas',
+        grupo: 'NÍVEL-F-GERENCIADO'
+    },
+    {
         nome: 'Aquisição',
         grupo: 'NÍVEL-F-GERENCIADO'
     },
-	{
+    {
         nome: 'Gerência de Configuração',
         grupo: 'NÍVEL-F-GERENCIADO'
     },
-	{
+    {
         nome: 'Garantia de Qualidade de Processo e Produto',
         grupo: 'NÍVEL-F-GERENCIADO'
     },
-	{
+    {
         nome: 'Medição e Análise',
         grupo: 'NÍVEL-F-GERENCIADO'
     },
-	{
+    {
         nome: 'Gerência de Portfólio de Projetos',
         grupo: 'NÍVEL-F-GERENCIADO'
     },
-	{
+    {
+        nome: 'Gerência de Operações e Serviços - primeira evolução',
+        grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
+    },
+    {
+        nome: 'Gerência de Operação de Mudanças',
+        grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
+    },
+    {
         nome: 'Gerência de Reutilização',
         grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
     },
@@ -35,78 +62,123 @@ var processos = [
         nome: 'Gerência de Projetos - primeira evolução',
         grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Gerência de Recursos Humanos',
         grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Definição do Processo Organizacional',
         grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Avaliação e Melhoria do processo organizacional',
         grupo: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
     },
-	{
+    {
+        nome: 'Desenvolvimento de Sistemas e Serviços',
+        grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
+    },
+    {
+        nome: 'Orçamento e Contabilização de Serviços',
+        grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
+    },
+    {
         nome: 'Desenvolvimento de Requisitos',
         grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Projeto e construção do Produto',
         grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Integração de Produto',
         grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Verificação',
         grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
     },
-	{
+    {
         nome: 'Validação',
         grupo: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
     },
-	{
+    {
+        nome: 'Gerência de Capacidade',
+        grupo: 'NÍVEL-C-DEFINIDO'
+    },
+    {
+        nome: 'Gerência de Continuidade e Disponibilidade',
+        grupo: 'NÍVEL-C-DEFINIDO'
+    },
+    {
+        nome: 'Gerência de Liberação',
+        grupo: 'NÍVEL-C-DEFINIDO'
+    },
+    {
+        nome: 'Gerência de Seguraça da Informação',
+        grupo: 'NÍVEL-C-DEFINIDO'
+    },
+    {
+        nome: 'Relato de Serviços',
+        grupo: 'NÍVEL-C-DEFINIDO'
+    },
+    {
         nome: 'Gerência de Decisões',
         grupo: 'NÍVEL-C-DEFINIDO'
     },
-	{
+    {
         nome: 'Gerência de Riscos',
         grupo: 'NÍVEL-C-DEFINIDO'
     },
-	{
+    {
         nome: 'Desenvolvimento para Reutilização',
         grupo: 'NÍVEL-C-DEFINIDO'
     },
-	{
+    {
         nome: 'Gerência de Projetos - Segunda evolução',
         grupo: 'NÍVEL-B-GERENCIADO-QUANTITATIVAMENTE'
+    },
+    {
+        nome: 'Gerência de Operação de Serviços - Segunda evolução',
+        grupo: 'NÍVEL-B-GERENCIADO-QUANTITATIVAMENTE'
+    },
+    {
+        nome: 'Um item, como, por exemplo, projeto (design), especificação, código-fonte, documentação, casos de teste, manuais, procedimentos, etc., que foi projetado para utilização em múltiplos contextos',
+        grupo: 'ATIVO-REUTILIZAVEL'
+    },
+    {
+        nome: 'Ativo reutilizável produzido a partir da engenharia de domínio',
+        grupo: 'ATIVO-DOMINIO'
     }
 ];
 
 
-var grupos = [
-    {
+var grupos = [{
         nome: 'NÍVEL-G-PARCIALMENTE-GERENCIADO'
     },
     {
         nome: 'NÍVEL-F-GERENCIADO'
-	},
+    },
     {
         nome: 'NÍVEL-E-PARCIALMENTE-DEFINIDO'
-	},
+    },
     {
         nome: 'NÍVEL-D-LARGAMENTE-DEFINIDO'
-	},
-	{
+    },
+    {
         nome: 'NÍVEL-C-DEFINIDO'
     },
-	{
+    {
         nome: 'NÍVEL-B-GERENCIADO-QUANTITATIVAMENTE'
     },
-	{
+    {
         nome: 'NÍVEL-A-EM-OTIMIZACAO'
+    },
+    {
+        nome: 'ATIVO-REUTILIZAVEL'
+    },
+    {
+        nome: 'ATIVO-DOMINIO'
     }
 ];
 

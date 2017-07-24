2adac2e6723bfa53a0966e811df9deb25580c205
Raphael Lacerda <lacerdaph@gmail.com>
2017-07-24 15:19:47 -0300

    competencias - constitucional

---
+++ processos/competencias.js
@@ -0,0 +1,238 @@
+var processos = [
+    {
+        nome: 'Não pode ser delegada',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Emissão de moeda',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Serviço postal',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Serviço estatística',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Serviço Geografia e Geologia',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Explorar Serviço de telecomunicações e rádio fusão',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Organização Judiciário DF',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Inspeção do Trabalho',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Defesa Nacional',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Explorar Portos Marítimos, fluviais',
+        grupo: 'EXCLUSIVA'
+    },
+    {
+        nome: 'Políticas de educação para segurança no trânsito',
+        grupo: 'COMUM'
+    },
+    {
+        nome: 'Pode delegar questões específicas por lei complementar',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Águas, energia',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Informática',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'telecomunicações e rádiofusão',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Comercial',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Agropecuário',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Penal',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Espacial',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Trabalho',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Eleitoral',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Seguridade Social',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Desapropriação',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Processual',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Marítmo',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Diretrizes para desenvolvimento urbano',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Diretrizes para Saneamento Básico',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Trânsito e Transporte',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Sistema de consórcio e sorteio',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Diretrizes e bases da Educação Nacional',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Propaganda Comercial',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Servidores',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Crimes de Responsabilidade',
+        grupo: 'PRIVATIVA'
+    },
+    {
+        nome: 'Penitenciário',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Urbanístico',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Tributário',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Orçamentário',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Financeiro',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Econômico',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Previdência Social',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'União faz normas gerais',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Estado pode fazer normas gerais que podem ser suspensas',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Ciência e Tecnologia',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Procedimentos em matéria processual',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Custas dos serviços forenses',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Juizado de pequenas causas',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Assistência jurídica',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Defensoria Pública',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Educação',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Cultura',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Ensino',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Desporto',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Juntas comerciais',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Direito do Consumidor',
+        grupo: 'CONCORRENTE'
+    },
+    {
+        nome: 'Proteção ao patrimônio histórico e cultural',
+        grupo: 'CONCORRENTE'
+    },
+
+];
+
+
+
+var grupos = [
+{
+    nome: 'EXCLUSIVA'
+}, {
+    nome: 'COMUM'
+}, {
+    nome: 'PRIVATIVA'
+}, {
+    nome: 'CONCORRENTE'
+}];
+
+var itensAcima = 2;

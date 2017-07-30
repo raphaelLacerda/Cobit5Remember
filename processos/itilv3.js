4f608c09391d39094382013d7ad43cabf02c4ec4
Raphael Lacerda <lacerdaph@gmail.com>
2017-07-30 17:06:48 -0300

    itil

--- processos/itilv3.js
+++ processos/itilv3.js
@@ -1,45 +1,60 @@
-var processos = [
-    {
+var processos = [{
         nome: 'Gestão Financeira',
         grupo: 'ESTRATÉGIA'
     },
-	{
+    {
         nome: 'Gerência de Portfólio',
         grupo: 'ESTRATÉGIA'
     },
-	{
+    {
         nome: 'Gestão de Demandas',
         grupo: 'ESTRATÉGIA'
     },
-	{
+    {
         nome: 'Gestão de Estratégia',
+        grupo: 'ESTRATÉGIA'
+    },
+    {
+        nome: 'Gestão de Relacionamento',
+        grupo: 'ESTRATÉGIA'
+    },
+    {
+        nome: 'Business Relationship Manager (BRM)',
+        grupo: 'ESTRATÉGIA'
+    },
+    {
+        nome: 'Product Manager (PM)',
+        grupo: 'ESTRATÉGIA'
+    },
+    {
+        nome: 'Chief Sourcing Officer (CSO)',
         grupo: 'ESTRATÉGIA'
     },
     {
         nome: 'Gestão de Nível de Serviços',
         grupo: 'DESENHO'
     },
-	{
+    {
         nome: 'Gestão de catálago de serviços',
         grupo: 'DESENHO'
     },
-	{
+    {
         nome: 'Gestão de Capacidade',
         grupo: 'DESENHO'
     },
-	{
+    {
         nome: 'Gestão de Disponibilidade',
         grupo: 'DESENHO'
     },
-	{
+    {
         nome: 'Gestão de Continuidade',
         grupo: 'DESENHO'
     },
-	{
+    {
         nome: 'Gestão de Segurança',
         grupo: 'DESENHO'
     },
-	{
+    {
         nome: 'Gestão de Fornecedores',
         grupo: 'DESENHO'
     },
@@ -47,27 +62,27 @@ var processos = [
         nome: 'Gestão do Conhecimento',
         grupo: 'TRANSIÇÃO'
     },
-	{
+    {
         nome: 'Gestão do Configuração e Ativos',
         grupo: 'TRANSIÇÃO'
     },
-	{
+    {
         nome: 'Gestão do Mudança',
         grupo: 'TRANSIÇÃO'
     },
-	{
+    {
         nome: 'Gestão do Liberação',
         grupo: 'TRANSIÇÃO'
     },
-	{
+    {
         nome: 'Validação e Teste de serviço',
         grupo: 'TRANSIÇÃO'
     },
-	{
+    {
         nome: 'Avaliação',
         grupo: 'TRANSIÇÃO'
     },
-	{
+    {
         nome: 'Planejamento e suporte de transição',
         grupo: 'TRANSIÇÃO'
     },
@@ -75,19 +90,19 @@ var processos = [
         nome: 'Gestão de Incidentes',
         grupo: 'OPERAÇÃO'
     },
-	{
+    {
         nome: 'Gestão de Problemas',
         grupo: 'OPERAÇÃO'
     },
-	{
+    {
         nome: 'Gestão de Acesso',
         grupo: 'OPERAÇÃO'
     },
-	{
+    {
         nome: 'Gestão de Evento',
         grupo: 'OPERAÇÃO'
     },
-	{
+    {
         nome: 'Cumprimeto de Requisição',
         grupo: 'OPERAÇÃO'
     },
@@ -95,15 +110,15 @@ var processos = [
         nome: 'Gestão Técnica',
         grupo: 'FUNÇÕES'
     },
-	{
+    {
         nome: 'Central de Serviços',
         grupo: 'FUNÇÕES'
     },
-	{
+    {
         nome: 'Gestão de Operações de TI',
         grupo: 'FUNÇÕES'
     },
-	{
+    {
         nome: 'Gestão de Aplicativo',
         grupo: 'FUNÇÕES'
     },
@@ -111,11 +126,11 @@ var processos = [
         nome: 'Melhoria em 7 passos',
         grupo: 'MELHORIA'
     },
-	{
+    {
         nome: 'Medição de Serviços',
         grupo: 'MELHORIA'
     },
-	{
+    {
         nome: 'Relatório de Serviços',
         grupo: 'MELHORIA'
     }
@@ -123,8 +138,7 @@ var processos = [
 ];
 
 
-var grupos = [
-    {
+var grupos = [{
         nome: 'ESTRATÉGIA'
     },
     {

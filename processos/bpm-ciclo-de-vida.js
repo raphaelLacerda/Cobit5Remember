2b090b4b9ea49cbc2f214994d53a5983b1c0ebd7
Raphael Lacerda <lacerdaph@gmail.com>
2017-08-26 17:37:34 -0300

    bpm ciclo de vida

---
+++ processos/bpm-ciclo-de-vida.js
@@ -0,0 +1,101 @@
+var processos = [{
+        nome: 'Definir as atividades de BPM que contribuirão para o alcance das metas organizacionais',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'Estabelecer estratégias, objetivos e abordagens para definir as mudanças',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'quais são os processos mais importantes para a estratégia da organização, e levantados seus pontos fracos,',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'Entender o ambiente externo e interno, além da estratégia organizacional',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'cria-se um ambiente de discussão entre partes envolvidas de forma a melhorar o processo em questão',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'Seleção de processos críticos',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'Priorização de atividades',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'Formação de equipes',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'diretrizes e especificações',
+        grupo: 'PLANEJAR'
+    },
+    {
+        nome: 'AS-IS, TO-BE',
+        grupo: 'MODELAR-OTIMIZAR'
+    },
+    {
+        nome: 'compreender e documentar os processos e coletar informações como custo, recursos e tempo necessários para a sua realização',
+        grupo: 'MODELAR-OTIMIZAR'
+    },
+    {
+        nome: 'melhores práticas, benchmarking',
+        grupo: 'MODELAR-OTIMIZAR'
+    },
+    {
+        nome: 'especificações para implantação',
+        grupo: 'MODELAR-OTIMIZAR'
+    },
+    {
+        nome: 'planos de transferência de tecnologia, treinamentos, ajustes de equipamentos, métodos e, se necessários, softwares',
+        grupo: 'IMPLANTACAO-PROCESSO'
+    },
+    {
+        nome: 'monitoria e controle da execução de instâncias de processo',
+        grupo: 'IMPLANTACAO-PROCESSO'
+    },
+    {
+        nome: 'Uso de indicadores - BAM, BI, BSC',
+        grupo: 'CONTROLE-ANALISE'
+    },
+    {
+        nome: 'presente durante todas as etapas da aplicação do BPM',
+        grupo: 'CONTROLE-ANALISE'
+    },
+    {
+        nome: 'gerando informações que posteriormente realimentarão as atividades de otimização e planejamento ',
+        grupo: 'CONTROLE-ANALISE'
+    },
+    {
+        nome: 'Controle de conjuntos de instâncias de processos',
+        grupo: 'CONTROLE-ANALISE'
+    },
+    {
+        nome: 'Auditoria',
+        grupo: 'CONTROLE-ANALISE'
+    },
+    {
+        nome: 'Rekatórios gerencias e dados para a estratégia',
+        grupo: 'CONTROLE-ANALISE'
+    },
+];
+
+
+
+var grupos = [{
+        nome: 'PLANEJAR'
+    },
+    {
+        nome: 'MODELAR-OTIMIZAR'
+    }, {
+        nome: 'IMPLANTACAO-PROCESSO'
+    }, {
+        nome: 'CONTROLE-ANALISE'
+    }
+];
+
+var itensAcima = 2;
